254f681f07e7671371bbbf898e9d1493aaa686b6
meghana039 <smeghanagowda@gmail.com>
2019-12-06 12:34:05 +0530

    Modifed

--- radiobutton.py
+++ radiobutton.py
@@ -1,6 +1,7 @@
 from idlelib import browser
+import time
 
 def radioClick(self):
     # _val = self.elementClick(self._radio_id, locatorType="id")
-    _val = browser.find_elements_by_xpath("//input[@type='radio'] ")
+    _val = browser.find_elements_by_xpath("//input[@type='radio']")
     print(_val)
